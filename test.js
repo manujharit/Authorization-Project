1e5e5c0770ed47a4aeddce6b7457c0ee601390c2
Manuj Haritwal <manuj.haritwal01@gmail.com>
2024-04-27 06:27:24 +0530

    remove unused file

--- test.js
+++
@@ -1,9 +0,0 @@
-const crypto = require('crypto');
-
-// Generate a random key of appropriate length (e.g., 256 bits)
-const key = crypto.randomBytes(32); // 32 bytes = 256 bits
-
-// Convert the key to a hexadecimal string
-const hashSecret = key.toString('hex');
-
-console.log(hashSecret);
